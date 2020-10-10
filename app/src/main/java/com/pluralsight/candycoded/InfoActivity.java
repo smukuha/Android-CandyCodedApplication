e3e08826f5cf9fec33d1616ef7870cfc7e36dcfe
smukuha <mukuhasafania@gmail.com>
2020-10-10 23:43:45 +0300

    Action View

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,5 +1,6 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -28,7 +29,9 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
     public void createMapIntent(View view){
-
+        Uri uriaddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriaddress);
+        mapIntent.setPackage("com.google.android.apps.maps");
     }
 
     // ***
